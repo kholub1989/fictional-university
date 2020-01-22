d9e95ba0e99cdf7208ba79609c441fd660cef9eb
Krisztian Holub <kholub1989@gmail.com>
2020-01-22 12:20:02 -0500

    dynamic and custom navigation menu

--- footer.php
+++ footer.php
@@ -14,6 +14,11 @@
         <div class="site-footer__col-two">
           <h3 class="headline headline--small">Explore</h3>
           <nav class="nav-list">
+          <?php 
+            // wp_nav_menu(array(
+            //   'theme_location' => 'footerLocationOne'
+            // ));
+          ?>
             <ul>
               <li><a href="<?php echo site_url('/about-us'); ?>">About Us</a></li>
               <li><a href="#">Programs</a></li>
@@ -26,6 +31,11 @@
         <div class="site-footer__col-three">
           <h3 class="headline headline--small">Learn</h3>
           <nav class="nav-list">
+          <?php 
+            // wp_nav_menu(array(
+            //   'theme_location' => 'footerLocationTwo'
+            // ));
+          ?>
             <ul>
               <li><a href="#">Legal</a></li>
               <li><a href="<?php echo site_url('/privacy-policy-2'); ?>">Privacy</a></li>
