45060899dc1477f00974699c9b7ae76715283ddb
Krisztian Holub <kholub1989@gmail.com>
2020-01-30 13:48:48 -0500

    feature image sizes and cropping

--- single-program.php
+++ single-program.php
@@ -47,7 +47,7 @@ the_post(); ?>
       $relatedProfessors->the_post(); ?>
       <li class="professor-card__list-item">
         <a class="professor-card" href="<?php the_permalink(); ?>">
-          <img class="professor-card__image" src="<?php the_post_thumbnail_url(); ?>" alt="<?php the_title(); ?>">
+          <img class="professor-card__image" src="<?php the_post_thumbnail_url('professorLandscape'); ?>" alt="<?php the_title(); ?>">
           <span class="professor-card__name"><?php the_title(); ?></span>
         </a>
       </li>
