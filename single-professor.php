a0149a6555f35f719453a1d3c7dee9f60904434e
Krisztian Holub <kholub1989@gmail.com>
2020-01-30 13:39:12 -0500

    featured image add post thumbnail

--- single-professor.php
+++ single-professor.php
@@ -17,7 +17,16 @@ the_post(); ?>
 
   <div class="container container--narrow page-section">
 
-  <div class="generic-content"><?php the_content(); ?></div>
+  <div class="generic-content">
+    <div class="row group">
+      <div class="one-third">
+        <?php the_post_thumbnail(); ?>
+      </div>
+      <div class="two-thirds">
+        <?php the_content(); ?>
+      </div>
+    </div>
+  </div>
 
 <?php 
   $relatedPrograms = get_field('related_programs');
