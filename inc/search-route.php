6fc5fcb5fda0f4a5e908c9ed6c2f6e22bfd2f182
Krisztian Holub <kholub1989@gmail.com>
2020-02-11 15:24:49 -0500

    add search logic that s aweare of relationships

--- inc/search-route.php
+++ inc/search-route.php
@@ -43,7 +43,8 @@
       if(get_post_type() == 'program') {
         array_push($results['programs'], array(
           'title' => get_the_title(),
-          'permalink' => get_the_permalink()
+          'permalink' => get_the_permalink(),
+          'id' => get_the_id()
         ));
       }
       if(get_post_type() == 'event') {
@@ -70,5 +71,36 @@
       }
     }
 
+    if ($results['programs']) {
+      $programsMetaQuery = array('relation' => 'OR');
+      
+      foreach($results['programs'] as $item) {
+        array_push($programsMetaQuery, array(
+          'key' => 'related_programs',
+          'compare' => 'LIKE',
+          'value' => '"' . $item['id'] . '"'
+        ));
+      }
+
+      $programRelationshipQuery = new WP_Query(array(
+        'post_type' => 'professor',
+        'meta_query' => $programsMetaQuery
+      ));
+
+      while ($programRelationshipQuery->have_posts()) {
+        $programRelationshipQuery->the_post();
+
+        if (get_post_type() == 'professor') {
+          array_push($results['professors'], array(
+            'title' => get_the_title(),
+            'permalink' => get_the_permalink(),
+            'image' => get_the_post_thumbnail_url(0, 'professorLandscape')
+          ));
+        }
+      }
+
+      $results['professors'] = array_values(array_unique($results['professors'], SORT_REGULAR));
+    }
+    
     return $results;
   }
