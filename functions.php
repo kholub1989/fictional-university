1b0240aacf2cdb8e7237710bce4ad61cfef9546e
Krisztian Holub <kholub1989@gmail.com>
2020-02-11 23:52:24 -0500

    adding user roles, permissions and open registration

--- functions.php
+++ functions.php
@@ -97,3 +97,42 @@ function universityMapKey($api) {
 
 add_filter('acf/fields/google_map/api', 'universityMapKey');
 
+// Redirect subscriber account out of admin and onto homepage
+add_action('admin_init', 'redirectSubsToFrontend');
+
+function redirectSubsToFrontend() {
+  $ourCurrentUser = wp_get_current_user();
+  if (count($ourCurrentUser->roles) == 1 AND $ourCurrentUser->roles[0] == 'subscriber') {
+    wp_redirect(site_url('/'));
+    exit;
+  }
+}
+
+add_action('wp_loaded', 'noSubsAdminBar');
+
+function noSubsAdminBar() {
+  $ourCurrentUser = wp_get_current_user();
+  if (count($ourCurrentUser->roles) == 1 AND $ourCurrentUser->roles[0] == 'subscriber') {
+    show_admin_bar(false);
+  }
+}
+
+// Customize Login Screen
+add_filter('login_headerurl', 'ourHeaderurl');
+
+function ourHeaderurl() {
+  return esc_url(site_url('/'));
+}
+
+add_action('login_enqueue_scripts', 'ourLoginCss');
+
+function ourLoginCss() {
+  wp_enqueue_style('university_main_styles', get_stylesheet_uri()); 
+  wp_enqueue_style('custom-google-fonts', '//fonts.googleapis.com/css?family=Roboto+Condensed:300,300i,400,400i,700,700i|Roboto:100,300,400,400i,700,700i');
+}
+
+add_filter('login_headertitle', 'ourLoginTitle');
+
+function ourLoginTitle() {
+  return get_bloginfo('name');
+}
